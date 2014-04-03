939300c9677182bb6b045ebfbe042acb534293f3
joanisaac.biel@gmail.com <ubuntu@ip-10-74-157-121.eu-west-1.compute.internal>
2014-04-03 10:50:34 +0000

    reduce app version

--- site/js/app.js
+++ site/js/app.js
@@ -1,23 +1,14 @@
-
-
 requirejs([
     'jquery',
     'underscore',
     'backbone',
-    'views/questionnaire',
     'views/form'],
-function($,_,Backbone,ViewQ,ViewF) {
+function($,_,Backbone,View) {
 
-
-    var questions = [{ id: 'Q1', type: 'video', 'title': 'Why do you want to do a phd?', time_wait: '30', time_response: '180' },
-       { id: 'Q2', type: 'text', title: 'What do you do to handle stress?', time_response: '120' }];
-
-    //console.log("... hardcoded questions " + questions);
-    //var appView = new View(questions);
-    var appView = new ViewF();
-    console.log("... loaded app.js");
+    var appView = new View();
+    //console.log("... loaded app.js");
   
- });
+});
 
 
     
